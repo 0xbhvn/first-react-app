d762f2285adef01707d7cc92871b20a763fbb7e2
Bhaven Kakade <thisisbhaven@gmail.com>
2020-06-22 00:57:33 +0530

    render props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import './App.css' 
-import ClickCounter from './components/ClickCounter'
-import HoverCounter from './components/HoverCounter'
+import User from './components/User'
+import CounterProp from './components/CounterProp'
+import ClickCounterTwo from './components/ClickCounterTwo'
+import HoverCounterTwo from './components/HoverCounterTwo'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ClickCounter name='Bhaven'/>
-        <HoverCounter />
+        <CounterProp render={ (count, incrementCount) => <ClickCounterTwo count={ count } incrementCount={ incrementCount } /> } />
+        <CounterProp render={ (count, incrementCount) => <HoverCounterTwo count={ count } incrementCount={ incrementCount } /> } />
+        <User render={ (isLoggedIn) => isLoggedIn ? 'Bhaven' : 'Guest' } />
       </div>
     )
   }
